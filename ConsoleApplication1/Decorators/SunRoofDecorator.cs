5aa9582a1d4f867769e2349cc8d85f7eb67c873e
matendie <matendie@gmail.com>
2016-05-02 15:43:09 -0400

    Restructured folders

---
+++ ConsoleApplication1/Decorators/SunRoofDecorator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    public class SunRoofDecorator : AccessoryDecorator
+    {
+        IAuto _auto;
+        public SunRoofDecorator(IAuto auto): base (auto)
+        {
+            _auto = auto;
+        }
+
+        public override void TurnOff()
+        {
+            Console.WriteLine(CarName + " is off, left with sun roof open.");
+        }
+
+        public override void TurnOn()
+        {
+            Console.WriteLine(CarName + " is off. Sunroof opening now.");
+        }
+    }
+}
